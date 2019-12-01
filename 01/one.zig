cf7692546e97815175208e49922792e77c191a65
Sander in 't Veld <sanderintveld@hotmail.com>
2019-12-01 18:01:02 +0100

    December 1, part one.

--- 01/one.zig
+++ 01/one.zig
@@ -6,13 +6,21 @@ pub fn main() !void {
     const input = try File.openRead("01/input.txt");
     defer input.close();
 
+    var total_fuel_req: u32 = 0;
+
     var strm = &input.inStream().stream;
     var buffer: [100]u8 = undefined;
     while (true) {
-        var line = try strm.readUntilDelimiterOrEof(buffer[0..], '\n');
-        if (line == null) break;
-        std.debug.warn("{}\n", line);
+        var maybe_line = try strm.readUntilDelimiterOrEof(buffer[0..], '\n');
+        if (maybe_line) |line| {
+            var mass: u32 = try fmt.parseInt(u32, line, 10);
+            var fuel: u32 = (mass / 3) - 2;
+            std.debug.warn("{} => mass: {}, fuel: {}\n", line, mass, fuel);
+            total_fuel_req += fuel;
+        } else {
+            break;
+        }
     }
 
-    std.debug.warn("Done.\n");
+    std.debug.warn("\nTotal fuel requirement: {}\n", total_fuel_req);
 }
