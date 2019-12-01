fc6cb801398fb5bfa050657559dd6489b8022e68
Sander in 't Veld <sanderintveld@hotmail.com>
2019-12-01 17:51:01 +0100

    Upgraded Zig to master; got reading the lines to work.

--- 01/one.zig
+++ 01/one.zig
@@ -6,11 +6,12 @@ pub fn main() !void {
     const input = try File.openRead("01/input.txt");
     defer input.close();
 
-    var strm = input.inStream().stream;
-    var buffer: []u8 = undefined;
-    while (strm.readUntilDelimiterOrEof(buffer, '\n')) {
-        try stdout.write(buffer);
-        try stdout.write("\n");
+    var strm = &input.inStream().stream;
+    var buffer: [100]u8 = undefined;
+    while (true) {
+        var line = try strm.readUntilDelimiterOrEof(buffer[0..], '\n');
+        if (line == null) break;
+        std.debug.warn("{}\n", line);
     }
 
     std.debug.warn("Done.\n");
