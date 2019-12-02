4daf8fb1377a1f77c7b8139815c86346d1055af4
Sander in 't Veld <sanderintveld@hotmail.com>
2019-12-02 11:25:40 +0100

    Simplified loading the program using std.mem.trim(); print the program.

--- 02/one.zig
+++ 02/one.zig
@@ -2,42 +2,44 @@ const std = @import("std");
 const fmt = @import("std").fmt;
 const File = std.fs.File;
 
-pub fn main() !void {
-    const input = try File.openRead("02/input-one.txt");
+fn load_program(program: *[1000]u32) !usize {
+    const input = try File.openRead("02/input.txt");
     defer input.close();
 
-    var total_fuel_req: u32 = 0;
-
-    var program = [_]u32{0} ** 1000;
-
-    var istrm = &input.inStream().stream;
-    var linebuffer: [12000]u8 = undefined;
+    const strm = &input.inStream().stream;
+    var buffer: [100]u8 = undefined;
     var pos: usize = 0;
     while (true) {
-        var mb_line = try istrm.readUntilDelimiterOrEof(linebuffer[0..], '\n');
-        if (mb_line) |line| {
-            std.debug.warn("({})\n", line);
-            var sistrm = std.io.SliceInStream.init(line);
-            var strm = sistrm.stream;
-            var buffer: [100]u8 = undefined;
-            while (true) {
-                var mb_str = try strm.readUntilDelimiterOrEof(buffer[0..], ',');
-                if (mb_str) |str| {
-                    std.debug.warn("[{}]\n", str);
-                    var opcode: u32 = try fmt.parseInt(u32, str, 10);
-                    std.debug.warn("{} => opcode: {}\n", str, opcode);
-                    program[pos] = opcode;
-                    pos += 1;
-                } else {
-                    break;
-                }
-            }
+        var mb_str = try strm.readUntilDelimiterOrEof(buffer[0..], ',');
+        if (mb_str) |str| {
+            const trimmed = std.mem.trim(u8, str, "\r\n ");
+            const opcode: u32 = try fmt.parseInt(u32, trimmed, 10);
+            program[pos] = opcode;
+            pos += 1;
         } else {
             break;
         }
     }
 
-    const len: usize = pos;
+    return pos;
+}
+
+fn print_program(program: []u32) void {
+    var pos: usize = 0;
+    while (pos < program.len) {
+        std.debug.warn("[{}] = {}\n", pos, program[pos]);
+        pos += 1;
+    }
+}
+
+pub fn main() !void {
+    var program = [_]u32{0} ** 1000;
+    const len: usize = try load_program(&program);
+
+    program[1] = 12;
+    program[2] = 02;
+
+    print_program(program[0..len]);
 
     std.debug.warn("\nCode in position 0: {}\n", program[0]);
 }
