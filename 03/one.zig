32da6f821877a159c46ad85335d7f727973a8a50
Sander in 't Veld <sanderintveld@hotmail.com>
2019-12-03 20:45:17 +0100

    Trying to parse snakes, but length 0.

--- 03/one.zig
+++ 03/one.zig
@@ -2,31 +2,57 @@ const std = @import("std");
 const fmt = @import("std").fmt;
 const File = std.fs.File;
 
-pub fn main() !void {
+const Move = struct {
+    way: u8,
+    dis: u32,
+};
+
+fn parse_snake(line: []u8) ![]Move {
+    var snake: [1000]Move = undefined;
+    var len: usize = 0;
+
+    var it = std.mem.tokenize(line, ",");
+    while (it.next()) |str| {
+        snake[len] = Move{
+            .way = str[0],
+            .dis = try fmt.parseInt(u32, str[1..], 10),
+        };
+    }
+
+    return snake[0..len];
+}
+
+fn parse_snakes(filename: []const u8) ![][]Move {
     const input = try File.openRead("03/sample1.txt");
     defer input.close();
 
-    var total_fuel_req: u32 = 0;
-
-    var program = [_]u32{0} ** 1000;
+    var snakes: [10][]Move = undefined;
+    var len: usize = 0;
 
     var istrm = &input.inStream().stream;
     var linebuffer: [1000]u8 = undefined;
-    var pos: usize = 0;
     while (true) {
         var mb_line = try istrm.readUntilDelimiterOrEof(linebuffer[0..], '\n');
         if (mb_line) |line| {
             std.debug.warn("({})\n", line);
-            var it = std.mem.tokenize(line, ",");
-            while (it.next()) |str| {
-                std.debug.warn("{}\n", str);
-            }
+            snakes[len] = try parse_snake(line);
+            len += 1;
         } else {
             break;
         }
     }
+    return snakes[0..len];
+}
 
-    const len: usize = pos;
+pub fn main() !void {
+    const snakes = try parse_snakes("03/sample1.txt");
+
+    for (snakes) |snake| {
+        for (snake) |move| {
+            std.debug.warn("{}{}", move.way, move.dis);
+        }
+        std.debug.warn(" ({})\n", snake.len);
+    }
 
     std.debug.warn("\nDone.\n");
 }
