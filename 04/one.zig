13452de21495e5971e19080fc29d4616e9fa39dd
Sander in 't Veld <sanderintveld@hotmail.com>
2019-12-07 12:41:39 +0100

    December 4, part one, first attempt.

---
+++ 04/one.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+const fmt = @import("std").fmt;
+const File = std.fs.File;
+
+pub fn main() void {
+    var n_matches: u32 = 0;
+
+    // The numbers have to be non-decreasing, so the first match higher or equal
+    // to 240298 is 244499.
+    var a: u8 = 2;
+    var b: u8 = 4;
+    var c: u8 = 4;
+    var d: u8 = 4;
+    var e: u8 = 9;
+    var f: u8 = 9;
+
+    all: while (a < 8) {
+        while (b < 10) {
+            while (c < 10) {
+                while (d < 10) {
+                    while (e < 10) {
+                        while (f < 10) {
+                            // The numbers have to be non-decreasing, so there
+                            // are no matches between 780000 and 784956.
+                            // In fact if we get here, we'll be at 788888.
+                            if (a == 7 and b == 8) {
+                                break :all;
+                            } else if (a == b or b == c or c == d or d == e or e == f) {
+                                //std.debug.warn("Found {}{}{}{}{}{}\n", a, b, c, d, e, f);
+                                n_matches += 1;
+                            }
+                            f += 1;
+                        }
+                        e += 1;
+                        f = e;
+                    }
+                    d += 1;
+                    e = d;
+                    f = d;
+                }
+                c += 1;
+                d = c;
+                e = c;
+                f = c;
+            }
+            b += 1;
+            c = b;
+            d = b;
+            e = b;
+            f = b;
+        }
+        a += 1;
+        b = a;
+        c = a;
+        d = a;
+        e = a;
+        f = a;
+    }
+
+    std.debug.warn("\n");
+    std.debug.warn("Number of matches found: {}\n", n_matches);
+}
